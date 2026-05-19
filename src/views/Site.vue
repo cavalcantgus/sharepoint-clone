a6547127b1d65e5cd22076a7d5efc613ef40fb7b
cavalcantgus <marcelo.cavalcanti.sousa@gmail.com>
2026-05-19 17:10:42 -0300

    modifying the folder view and touch functionality

--- src/views/Site.vue
+++ src/views/Site.vue
@@ -6,18 +6,18 @@
                     <span class="text-headline-medium">{{ site?.displayName }}</span>
                 </v-col>
             </v-row>
-            <v-row class="d-flex align-center border">
+            <v-row class="d-flex align-center">
                 <v-col cols="9" lg="12">
                     <v-text-field hide-details prepend-inner-icon="mdi-magnify" variant="outlined" density="compact"
                         placeholder="Buscar pastas ou arquivos"></v-text-field>
                 </v-col>
-                <v-col v-if="isMobile" cols="3" class="border">
+                <v-col v-if="isMobile" cols="3" class="d-flex justify-end">
                     <v-btn variant="outlined" color="primary" class="">
                         <v-icon size="25">mdi-filter-variant</v-icon>
                     </v-btn>
                 </v-col>
             </v-row>
-            <v-row dense no-gutters v-if="!isMobile">
+            <v-row class="pt-3" no-gutters v-if="!isMobile">
                 <v-col cols="4" lg="2">
                     <v-select class="type-select" label="Tipo" variant="outlined" density="compact" />
                 </v-col>
@@ -29,7 +29,19 @@
                 </v-col>
             </v-row>
 
-            <v-row class="mt-4">
+            <v-row v-if="!isMobile">
+                <v-col cols="12" class="d-flex justify-end ga-2">
+                    <v-btn class="elevation-0">
+                        <v-icon size="20">mdi-land-rows-horizontal</v-icon>
+                    </v-btn>
+                    <v-divider vertical></v-divider>
+                    <v-btn class="elevation-0">
+                        <v-icon size="20">mdi-view-grid-outline</v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+
+            <v-row class="mt-4" v-if="!isMobile">
                 <v-col cols="12">
                     <div v-if="loadingFolder" class="d-flex justify-center align-center py-8">
                         <v-progress-circular indeterminate color="#87d209" size="48" />
@@ -80,13 +92,48 @@
                                         <v-list-item variant="elevated"
                                             v-for="opcao in item.tipo === 'pasta' ? opcoesPasta : opcoesArquivo"
                                             :key="opcao.label" @click="opcao.acao(item)">
-                                            <v-list-item-title>{{ opcao.label }}</v-list-item-title>
+                                            <v-list-item-title class="d-flex ga-3">
+                                                <v-icon>{{ opcao.icon }}</v-icon>
+                                                <span>{{ opcao.label }}</span>
+                                            </v-list-item-title>
                                         </v-list-item>
                                     </v-menu>
                                 </td>
                             </tr>
                         </tbody>
                     </v-table>
+                </v-col>
+            </v-row>
+
+            <!-- Grid de Pastas e Arquivos -->
+            <v-row class="mt-6" v-if="!loadingFolder && folderData.length > 0 && isMobile">
+                <v-col v-for="(item, index) in folderData" :key="index" cols="6" sm="4" md="3" lg="2">
+                    <!-- Card principal com hover e ação de clique -->
+                    <v-card variant="elevated"
+                        class="pa-2 d-flex flex-column align-center justify-center text-center cursor-pointer item-card fill-height"
+                        @click="fetchSubFolders(item.ServerRelativeUrl)"
+                        @touchstart="startLongPress(item)"
+                        @touchend="cancelLongPress"
+                        @touchmove="cancelLongPress">
+                        <!-- Ícone Grande Dinâmico -->
+                        <v-card-title class="d-flex justify-end w-100 pa-0">
+                            <v-icon class="">mdi-dots-vertical</v-icon>
+                        </v-card-title>
+                        <v-icon size="56" :color="item.tipo === 'pasta' ? 'amber-darken-1' : 'blue-accent-2'"
+                            class="mb-3">
+                            {{ item.tipo === 'pasta' ? 'mdi-folder' : 'mdi-file-document-outline' }}
+                        </v-icon>
+
+                        <!-- Nome do Item (Tratado para não quebrar o layout com nomes longos) -->
+                        <div class="text-body-2 font-weight-medium text-truncate w-100 px-1">
+                            {{ item.Name }}
+                        </div>
+
+                        <!-- Subtítulo opcional de metadados se necessário -->
+                        <div class="text-caption text-grey mt-1 text-truncate w-100">
+                            {{ item.tipo === 'pasta' ? 'Pasta' : 'Arquivo' }}
+                        </div>
+                    </v-card>
                 </v-col>
             </v-row>
         </v-sheet>
@@ -100,6 +147,8 @@ import { authService } from '../auth/authService';
 export default {
     name: 'SiteDetails',
     data: () => ({
+        timer: null,
+        longPressed: false,
         folderData: null,
         loadingFolder: false,
         header: [
@@ -122,16 +171,18 @@ export default {
 
         opcoesPasta() {
             return [
-                { label: 'Abrir', acao: this.abrirPasta },
-                { label: 'Renomear', acao: this.renomearPasta },
-                { label: 'Deletar', acao: this.deletarPasta },
+                { label: 'Abrir', icon: 'mdi-folder-open-outline', acao: this.abrirPasta },
+                { label: 'Renomear', icon: 'mdi-pencil-outline', acao: this.renomearPasta },
+                { label: 'Deletar', icon: 'mdi-trash-can-outline', acao: this.deletarPasta },
             ]
         },
+
         opcoesArquivo() {
             return [
-                { label: 'Baixar', acao: this.baixarArquivo },
-                { label: 'Visualizar', acao: this.visualizarArquivo },
-                { label: 'Deletar', acao: this.deletarArquivo },
+                { label: 'Baixar', icon: 'mdi-tray-arrow-down', acao: this.baixarArquivo },
+                { label: 'Visualizar', icon: 'mdi-eye', acao: this.visualizarArquivo },
+                { label: 'Deletar', icon: 'mdi-trash-can-outline', acao: this.deletarArquivo },
+                { label: 'Comentar', icon: 'mdi-comment-text-outline', acao: this.deletarArquivo },
             ]
         }
     },
@@ -156,6 +207,24 @@ export default {
     },
 
     methods: {
+
+        startLongPress(item) {
+            this.longPressed = false
+
+            this.timer = setTimeout(() => {
+
+                this.longPressed = true
+
+                navigator.vibrate?.(50)
+
+                item.menu = true
+
+            }, 500)
+        },
+
+        cancelLongPress() {
+            clearTimeout(this.timer)
+        },
         async fetchSharePointFolder() {
             this.loadingFolder = true;
             try {
